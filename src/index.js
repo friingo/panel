202ca0304b36828328474e67b52c0d3ca8a24221
yolocat-dev <developer@yolocat.dev>
2022-10-31 13:37:28 +0100

    Create React App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< Updated upstream
 import App from './components/App';
+=======
+import './index.css';
+import App from './App';
+>>>>>>> Stashed changes
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<<<<<<< Updated upstream
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
@@ -13,3 +19,14 @@ root.render(
 // REMOVE BEFORE PRODUCTION //
 reportWebVitals();
 // REMOVE BEFORE PRODUCTION //
+=======
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+>>>>>>> Stashed changes
